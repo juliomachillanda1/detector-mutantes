95164793e22db7d81b6626b70d9de914623a2375
Julio Machillanda <juliomachillanda1@gmail.com>
2020-06-13 19:11:37 -0300

    Configuro Spring Boot.

---
+++ src/main/java/com/juliomachillanda/detectormutantes/DetectorMutantesApplication.java
@@ -0,0 +1,13 @@
+package com.juliomachillanda.detectormutantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectorMutantesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DetectorMutantesApplication.class, args);
+    }
+
+}
